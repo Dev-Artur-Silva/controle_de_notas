071fdc25c41e6906bc32be6a71c09f2fb21a2235
Artur Silva <artur_antoniopw01@hotmail.com>
2020-11-21 02:41:03 -0300

    Mudança de variáveis e adicionado relatorio
    Mudei o nome de algumas variáveis, corrigi alguns erros básicos e adicionei a função de relatório.

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,26 @@ def cadastrar_disciplina(excel_disciplinas, codigo, nome, matricula_professor):
 def cadastrar_notas(excel_notas, codigo, matricula_aluno, nota1, nota2):
     linha = [codigo, matricula_aluno, nota1, nota2]
     excel_notas.loc[len(excel_notas)] = linha
+
+def relatorio_notas(excel_disciplinas, excel_notas, excel_professores, excel_alunos):
+    #Imprimir a disciplina, professor da disciplina, relação dos alunos e
+    #suas notas finais (média da Nota 1 e Nota 2).
+    for posicao_disciplina, linha_disciplina in excel_disciplinas.iterrows():
+        print()
+        for posicao_professor, linha_professor in excel_professores.iterrows():
+            
+            for posicao_notas, linha_notas in excel_notas.iterrows():
+
+                for posicao_alunos, linha_alunos in excel_alunos.iterrows():
+
+                    if linha_disciplina['Matricula do Professor'] == linha_professor['Matricula']:
+
+                        print('Disciplina:', linha_disciplina['Nome'],', Nome do Professor:',linha_professor['Nome'])
+
+                        if linha_disciplina['Codigo'] == linha_notas['Codigo da Disciplina']:
+
+                            if linha_notas['Matricula do Aluno'] == linha_alunos['Matricula']:
+
+                                nota_final = (linha_notas['Nota 1'] + linha_notas['Nota 2']) / 2
+
+                                print('Nome do Aluno:',linha_alunos['Nome'],', Nota final:',nota_final)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import funcoes
 
-arquivo = pd.ExcelFile("C:aps_excel.xlsx")
+arquivo = pd.ExcelFile("C:dados.xlsx")
 excel_professores = pd.read_excel(arquivo, 'Professores')
 excel_alunos = pd.read_excel(arquivo, 'Alunos')
 excel_notas = pd.read_excel(arquivo, 'Notas')
@@ -34,7 +34,7 @@ def menu():
         if (menu == 1):
             #Cadastro de Professores (Nome, matrícula, data de nascimento)
             nome = input('Digite o nome do Professor: ')
-            matricula = int(input('Digite a matricula do Professor'))
+            matricula = int(input('Digite a matricula do Professor: '))
             data = input('Digite a data de nascimento (dd/mm/aaaa): ')
             
             funcoes.cadastrar_professor(excel_professores, nome, matricula, data)
@@ -42,7 +42,7 @@ def menu():
         elif (menu == 2):
             #Cadastro de Alunos (Nome, matrícula, data de nascimento)
             nome = input('Digite o nome do Aluno: ')
-            matricula = int(input('Digite a matricula do Aluno'))
+            matricula = int(input('Digite a matricula do Aluno: '))
             data = input('Digite a data de nascimento (dd/mm/aaaa): ')
 
             funcoes.cadastrar_aluno(excel_alunos, nome, matricula, data)
@@ -58,15 +58,14 @@ def menu():
         elif (menu == 4):
             #Cadastro de Notas (Código da Disciplina, matrícula do aluno, Nota 1, Nota2)
             codigo = int(input('Digite o codigo da disciplina: '))
-            matricula = input('Digite a matricula do Professor: ')
+            matricula = int(input('Digite a matricula do Aluno: '))
             nota1 = float(input('Digite a 1ª nota do aluno: '))
             nota2 = float(input('Digite a 2ª nota do aluno: '))
             
             funcoes.cadastrar_notas(excel_notas, codigo, matricula, nota1, nota2)
 
-
         elif (menu == 5):
                 #o Imprimir a disciplina, professor da disciplina, relação dos alunos e
                 #suas notas finais (média da Nota 1 e Nota 2).
-            pass
+            funcoes.relatorio_notas(excel_disciplinas, excel_notas, excel_professores, excel_alunos)
 menu()
